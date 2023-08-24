1b2dcfcd8255c814eec3c826f0a6f508a0e9129c
naresh8555890808 <141204145+naresh8555890808@users.noreply.github.com>
2023-08-24 22:41:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function seating(s,r){
+    if(s==1){
+       r==1
+       return(s ,r+ 
+        " is lower berth");
+       }else if(s==2){
+        r==2
+        return(s ,r+ 
+         " is lower berth");
+       }else if(s==54){
+        r==7
+            
+         return(s ,r + 
+          " is upper berth")
+
+         }
+}
+console.log(seating(54,7))
